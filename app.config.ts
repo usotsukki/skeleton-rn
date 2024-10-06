47e75c2248333580a9f824605573ddd69dceed8a
vlad.shkelebei <v.shk.business@gmail.com>
2024-10-06 12:04:09 +0300

    feat: customized app config

---
+++ app.config.ts
@@ -0,0 +1,36 @@
+import { ConfigContext, ExpoConfig } from 'expo/config'
+
+export default ({ config: initConfig }: ConfigContext): ExpoConfig => {
+	const appName = initConfig?.name || 'Skeleton'
+	const environment = process.env.NODE_ENV as 'production' | 'development' | 'testing'
+
+	const name = {
+		production: appName,
+		testing: `${appName} Testing`,
+		development: `${appName} Development`,
+	}[environment]
+
+	const bundleIdentifier = environment === 'testing' ? 'com.skeleton.test' : 'com.skeleton'
+
+	return {
+		...initConfig,
+		name,
+		slug: initConfig?.slug || 'skeleton',
+		userInterfaceStyle: environment === 'development' ? 'dark' : 'automatic',
+		ios: {
+			...initConfig.ios,
+			bundleIdentifier,
+			config: {
+				googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY_IOS,
+			},
+		},
+		android: {
+			package: bundleIdentifier,
+		},
+		extra: {
+			eas: {
+				projectId: '6afd57d1-5bac-48fc-b3f3-40a510289ae2',
+			},
+		},
+	}
+}
--- src/env/index.ts
+++ src/env/index.ts
@@ -12,8 +12,8 @@ export const web = Platform.OS === 'web'
 export const IS_WEB = web
 
 export const IS_DEV = (typeof __DEV__ === 'boolean' && __DEV__) || !!Number(ENABLE_DEV_MODE)
-export const IS_TEST = process.env.EXPO_PUBLIC_IS_TESTING === 'true'
-export const IS_PROD = !IS_DEV && !IS_TEST
+export const IS_TEST = process.env.EXPO_PUBLIC_NODE_ENV === 'testing'
+export const IS_PROD = process.env.EXPO_PUBLIC_NODE_ENV === 'production' && !IS_DEV && !IS_TEST
 
 export const LOG_DEBUG = process.env.EXPO_PUBLIC_LOG_DEBUG !== 'false' ? process.env.EXPO_PUBLIC_LOG_DEBUG : ''
 export const LOG_LEVEL = process.env.EXPO_PUBLIC_LOG_LEVEL
